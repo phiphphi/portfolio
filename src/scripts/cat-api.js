1d0d4c1693d77eade1f01186e9ad74c0037b6b37
Phi <pton@uw.edu>
2021-12-22 15:21:19 -0800

    Working cat button

--- src/scripts/cat-api.js
+++ src/scripts/cat-api.js
@@ -19,35 +19,37 @@ function ajax_get(url, callback) {
     xmlhttp.send();
 }
 
-// Update page with gorgeous cat pictures.
-// ajax_get('https://api.thecatapi.com/v1/images/search?size=full', function (data) {
-//     document.getElementById("a1").innerHTML = data[0]["id"];
-//     document.getElementById("a2").innerHTML = data[0]["url"];
 
-//     var html = '<img src="' + data[0]["url"] + '">';
-//     document.getElementById("a3").innerHTML = html;
-// });
-
-
-// Get the modal
+// Get the modal.
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("cat-btn");
+// Get the button that opens the modal.
+var btn = document.getElementById("catBtn");
 
-// Get the <span> element that closes the modal
+// Get the <span> element that closes the modal.
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function () {
-    modal.style.display = "block";
-}
+var header = document.getElementById("header");
 
-// When the user clicks on <span> (x), close the modal
+header.addEventListener('click', function () {
+    modal.style.display = "block";
+
+    // Update page with gorgeous cat pictures.
+    ajax_get('https://api.thecatapi.com/v1/images/search?size=full', function (data) {
+        document.getElementById("a1").innerHTML = data[0]["id"];
+        document.getElementById("a2").innerHTML = data[0]["url"];
+
+        var html = '<img src="' + data[0]["url"] + '">';
+        document.getElementById("a3").innerHTML = html;
+    });
+});
+
+// When the user clicks on <span> (x), close the modal.
 span.onclick = function () {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close it.
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
--- src/scripts/startup.js
+++ src/scripts/startup.js
@@ -1,18 +1,16 @@
 function addPageLayout() {
-    load("../html/header.html", document.getElementById("header"));
     load("../html/footer.html", document.getElementById("footer"));
 }
 
 // Grab the html from the url and load it into the given element.
-function load(url, element)
-{
+function load(url, element) {
     fetch(url)
-    .then(data => {
-      return data.text()
-    })
-    .then(data => {
-        element.innerHTML = data;
-    })
+        .then(data => {
+            return data.text()
+        })
+        .then(data => {
+            element.innerHTML = data;
+        })
 }
 
 addPageLayout();
